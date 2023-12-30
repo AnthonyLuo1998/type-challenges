49df21a03e49cb0a99fe6fd5283cd1c2b81e5278
AnthonyLuo1998 <jum1274001055@gmail.com>
2023-12-31 01:20:23 +0800

    00116-medium-replace

---
+++ 00116-medium-replace/template.ts
@@ -0,0 +1,11 @@
+type Replace<
+  S extends string,
+  From extends string,
+  To extends string
+> = From extends ""
+  ? S
+  : S extends `${infer X}${From}${infer Y}`
+  ? `${X}${To}${Y}`
+  : S;
+
+type A = Replace<"foofoobar", "foo", "bar">;
